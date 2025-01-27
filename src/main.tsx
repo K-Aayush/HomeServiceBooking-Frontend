eb22fff4901cea23693cd889ec876c94b496db2b
Aayush <ayushkarkee6@gmail.com>
2025-01-27 22:14:42 +0545

    update :: adding the publishablekey in main.tsx of clerk

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.tsx";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AppContextProvider>
