44ded5c55bdd9f43ee0f6af8fff8bf8925067d0e
Aayush <ayushkarkee6@gmail.com>
2025-01-27 21:24:41 +0545

    update :: completed navbar style for desktop screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     <div className="flex shadow-sm p-5 justify-between">
       <div className="flex items-center gap-8">
         <img src={logo} alt="logo" width={200} height={200} />
-        <div className="flex items-center gap-6">
-          <h2>Home</h2>
-          <h2>Services</h2>
-          <h2>About Us</h2>
+        <div className="md:flex hidden items-center gap-6">
+          <h2 className="hover:scale-105 hover:text-primary">Home</h2>
+          <h2 className="hover:scale-105 hover:text-primary">Services</h2>
+          <h2 className="hover:scale-105 hover:text-primary">About Us</h2>
         </div>
       </div>
-      <div className="flex gap-2">
+      <div className="md:flex hidden gap-2">
         <Button variant="ghost">Recuriter Login</Button>
         <Button>Login</Button>
       </div>
