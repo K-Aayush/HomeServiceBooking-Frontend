264aa7b76af018a348e9ebbe5ae504fc528c2c64
Aayush <ayushkarkee6@gmail.com>
2025-01-27 23:21:19 +0545

    update :: state to close the side menu when navigating different pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,22 @@ import { MenuIcon } from "lucide-react";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { useState } from "react";
 import { ClipLoader } from "react-spinners";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user, isLoaded } = useUser();
   const [isSheetOpen, setIsSheetOpen] = useState<boolean>(false);
 
+  //close the sidebar when opening the signin popup
   const handleSignIn = () => {
     setIsSheetOpen(false);
     openSignIn();
+  };
+
+  //close the sidebar when navigating the pages
+  const handleCloseSheet = () => {
+    setIsSheetOpen(false);
   };
   return (
     <div className="flex shadow-sm p-5 justify-between">
@@ -22,9 +30,15 @@ const Navbar = () => {
         {/* navLinks */}
 
         <div className="md:flex hidden items-center gap-6">
-          <h2 className="hover:scale-105 hover:text-primary">Home</h2>
-          <h2 className="hover:scale-105 hover:text-primary">Services</h2>
-          <h2 className="hover:scale-105 hover:text-primary">About Us</h2>
+          <Link to={"/"}>
+            <h2 className="hover:scale-105 hover:text-primary">Home</h2>
+          </Link>
+          <Link to={"/Services"}>
+            <h2 className="hover:scale-105 hover:text-primary">Services</h2>
+          </Link>
+          <Link to={"/About"}>
+            <h2 className="hover:scale-105 hover:text-primary">About Us</h2>
+          </Link>
         </div>
       </div>
       {!isLoaded ? (
@@ -52,13 +66,21 @@ const Navbar = () => {
           <SheetContent>
             <div className="flex flex-col gap-2 mt-10">
               <div className="flex flex-col gap-2 text-center">
-                <h2 className="p-2 border border-gray-600 rounded-md">Home</h2>
-                <h2 className="p-2 border border-gray-600 rounded-md">
-                  Service
-                </h2>
-                <h2 className="p-2 border border-gray-600 rounded-md">
-                  About Us
-                </h2>
+                <Link to={"/"} onClick={handleCloseSheet}>
+                  <h2 className="p-2 border border-gray-600 rounded-md">
+                    Home
+                  </h2>
+                </Link>
+                <Link to={"/Services"} onClick={handleCloseSheet}>
+                  <h2 className="p-2 border border-gray-600 rounded-md">
+                    Service
+                  </h2>
+                </Link>
+                <Link to={"/About"} onClick={handleCloseSheet}>
+                  <h2 className="p-2 border border-gray-600 rounded-md">
+                    About Us
+                  </h2>
+                </Link>
               </div>
               {user ? (
                 <div></div>
