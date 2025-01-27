ef3d2624790db3ac7ef2d5ec0e3dbef5579c3bc2
Aayush <ayushkarkee6@gmail.com>
2025-01-27 21:20:51 +0545

    update :: made the alignment of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import logo from "../assets/Logo.svg";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex shadow-sm p-5 justify-between">
+      <div className="flex items-center gap-8">
+        <img src={logo} alt="logo" width={200} height={200} />
+        <div className="flex items-center gap-6">
+          <h2>Home</h2>
+          <h2>Services</h2>
+          <h2>About Us</h2>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <Button variant="ghost">Recuriter Login</Button>
+        <Button>Login</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
