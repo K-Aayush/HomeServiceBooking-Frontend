1feef1cf970f1708bde698411270b006554eee9f
Aayush <ayushkarkee6@gmail.com>
2025-01-27 23:39:55 +0545

    update :: complete header of hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="font-bold text-[46px] text-center">
+        Find Home <span className="text-primary">Service/Repair</span> <br />
+        Near You
+      </h2>
+      <h2 className="text-xl text-gray-400">
+        Explore Best Home Service & Repair Near you
+      </h2>
+    </div>
+  );
 };
 
 export default Hero;
