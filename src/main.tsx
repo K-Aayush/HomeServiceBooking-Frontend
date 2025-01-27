c21089f1b840b1ed1acf7b2927739029940d8be7
Aayush <ayushkarkee6@gmail.com>
2025-01-28 00:12:38 +0545

    creating new filr AppContextProvider and changing the of AppContext for better folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { AppContextProvider } from "./context/AppContext.tsx";
+import { AppContextProvider } from "./context/AppContextProvider.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
