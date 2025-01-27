e6b5db96bf8610b2dce8c60f9b685c3187ba2d93
Aayush <ayushkarkee6@gmail.com>
2025-01-28 00:22:10 +0545

    update :: using search functionality using useref and usecontext and shown in console

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,22 @@
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Search } from "lucide-react";
+import { useContext, useRef } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Hero = () => {
+  const { setIsSearched, setSearchFilter } = useContext(AppContext);
+
+  //ref value for input
+  const titleRef = useRef<HTMLInputElement>(null!);
+
+  //onsearch function
+  const onSearch = () => {
+    setSearchFilter({
+      title: titleRef.current.value,
+    });
+    setIsSearched(true);
+  };
   return (
     <div className="flex flex-col gap-3 items-center justify-center">
       <h2 className="font-bold text-[46px] text-center">
@@ -13,8 +27,12 @@ const Hero = () => {
         Explore Best Home Service & Repair Near you
       </h2>
       <div className="flex gap-4 items-center mt-4">
-        <Input placeholder="Search" className="rounded-full md:w-[350px]" />
-        <Button className="rounded-full h-12">
+        <Input
+          ref={titleRef}
+          placeholder="Search"
+          className="rounded-full md:w-[350px]"
+        />
+        <Button onClick={onSearch} className="rounded-full h-12">
           <Search className="w-4 h-4" />
         </Button>
       </div>
