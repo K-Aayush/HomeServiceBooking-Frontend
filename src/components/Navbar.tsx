076d8cfaf3dc41e0d43c629322267502f2b939fb
Aayush <ayushkarkee6@gmail.com>
2025-01-27 21:27:32 +0545

    update :: creating nav for mobile screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,29 @@
 import logo from "../assets/Logo.svg";
 import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="flex shadow-sm p-5 justify-between">
       <div className="flex items-center gap-8">
+        {/* logo */}
         <img src={logo} alt="logo" width={200} height={200} />
+        {/* navlinks */}
         <div className="md:flex hidden items-center gap-6">
           <h2 className="hover:scale-105 hover:text-primary">Home</h2>
           <h2 className="hover:scale-105 hover:text-primary">Services</h2>
           <h2 className="hover:scale-105 hover:text-primary">About Us</h2>
         </div>
       </div>
+      {/* Auth links */}
       <div className="md:flex hidden gap-2">
         <Button variant="ghost">Recuriter Login</Button>
         <Button>Login</Button>
+      </div>
+
+      {/* mobile screen */}
+      <div className="flex md:hidden">
+        <MenuIcon size={25} />
       </div>
     </div>
   );
