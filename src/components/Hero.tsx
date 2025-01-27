357982deb16694fe8808ba850d2feeb822ba84d6
Aayush <ayushkarkee6@gmail.com>
2025-01-27 23:46:31 +0545

    update :: styled input and used search icon as button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col gap-3 items-center justify-center">
       <h2 className="font-bold text-[46px] text-center">
         Find Home <span className="text-primary">Service/Repair</span> <br />
         Near You
@@ -10,6 +13,12 @@ const Hero = () => {
       <h2 className="text-xl text-gray-400">
         Explore Best Home Service & Repair Near you
       </h2>
+      <div className="flex gap-4 items-center mt-4">
+        <Input placeholder="Search" className="rounded-full md:w-[350px]" />
+        <Button className="rounded-full h-12">
+          <Search className="w-4 h-4" />
+        </Button>
+      </div>
     </div>
   );
 };
