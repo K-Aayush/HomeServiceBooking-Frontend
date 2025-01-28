2ecbfff67e417578efe67c4dad2647b25d3c073b
Aayush <ayushkarkee6@gmail.com>
2025-01-28 16:52:23 +0545

    update :: mapped the category list adn displayed

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,5 +1,16 @@
+import { BusinessCategories } from "../lib/data";
+
 const CategoryList = () => {
-  return <div>CategoryList</div>;
+  return (
+    <div className="mx-4 md:mx-22 lg:mx-52 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      {BusinessCategories.map((item, index) => (
+        <div className="flex flex-col items-center justify-center gap-2">
+          <img src={item.icon} alt="icon" width={35} height={35} />
+          <h2>{item.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryList;
