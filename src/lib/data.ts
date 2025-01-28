b5eda83432400c41bead75f12004402f90949184
Aayush <ayushkarkee6@gmail.com>
2025-01-28 16:20:48 +0545

    update :: made the neew export data of business categories

---
+++ src/lib/data.ts
@@ -0,0 +1,39 @@
+import mop from "../assets/mop.png";
+import cargo from "../assets/cargo-truck.png";
+import electricity from "../assets/electricity.png";
+import brush from "../assets/paintbrush.png";
+import repair from "../assets/repairing.png";
+import support from "../assets/support.png";
+
+export const BusinessCategories = [
+  {
+    id: 1,
+    name: "Cleaning",
+    icon: mop,
+  },
+  {
+    id: 2,
+    name: "Repair",
+    icon: repair,
+  },
+  {
+    id: 3,
+    name: "Painting",
+    icon: brush,
+  },
+  {
+    id: 4,
+    name: "Shifting",
+    icon: cargo,
+  },
+  {
+    id: 5,
+    name: "Plumbing",
+    icon: support,
+  },
+  {
+    id: 6,
+    name: "Electric",
+    icon: electricity,
+  },
+];
