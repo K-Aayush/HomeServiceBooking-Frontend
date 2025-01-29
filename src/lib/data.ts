a4936767649079e20edcce935259a12ec89b985d
Aayush <ayushkarkee6@gmail.com>
2025-01-29 13:01:32 +0545

    update :: creating a businessList Data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,6 +4,10 @@ import electricity from "../assets/electricity.png";
 import brush from "../assets/paintbrush.png";
 import repair from "../assets/repairing.png";
 import support from "../assets/support.png";
+import paint from "../assets/paint.jpg";
+import repairing from "../assets/repair.jpg";
+import women from "../assets/women.jpg";
+import women1 from "../assets/women1.jpg";
 
 export const BusinessCategories = [
   {
@@ -35,5 +39,50 @@ export const BusinessCategories = [
     id: 6,
     name: "Electric",
     icon: electricity,
+  },
+];
+
+export const BusinessList = [
+  {
+    id: 1,
+    name: "House Cleaning",
+    about: "",
+    address: "itahari, Sunsari",
+    cateory: {
+      name: "Cleaning",
+    },
+    contactPerson: "Aayusha Karkee",
+    email: "aayusha@gmail.com",
+    images: {
+      url: [women, women1],
+    },
+  },
+  {
+    id: 2,
+    name: "House Reparing",
+    about: "",
+    address: "Dharan, Sunsari",
+    cateory: {
+      name: "Repair",
+    },
+    contactPerson: "Pratham Neupane",
+    email: "pratham@gmail.com",
+    images: {
+      url: [repairing],
+    },
+  },
+  {
+    id: 3,
+    name: "House Painting",
+    about: "",
+    address: "Biratnagar, Morang",
+    cateory: {
+      name: "painting",
+    },
+    contactPerson: "Aadarsha Dhungana",
+    email: "adarsha@gmail.com",
+    images: {
+      url: [paint],
+    },
   },
 ];
