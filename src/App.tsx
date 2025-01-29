6f53f5e953884347a50856b6f80e8235a1d1f0d5
Aayush <ayushkarkee6@gmail.com>
2025-01-29 16:44:33 +0545

    update :: creating new component footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/About" element={<About />} />
         <Route path="/Services" element={<Services />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
