d85e291fe04e349accae9253ed2faf7229d2c586
Aayush <ayushkarkee6@gmail.com>
2025-01-29 13:23:03 +0545

    update :: added hover effect and smooth animation in businesslist component

--- src/components/BusinessList.tsx
+++ src/components/BusinessList.tsx
@@ -1,4 +1,5 @@
 import { PopularBusinessList } from "../lib/data";
+import { Button } from "./ui/button";
 
 const BusinessList = () => {
   return (
@@ -6,7 +7,10 @@ const BusinessList = () => {
       <h2 className="text-2xl font-bold">Popular Business</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
         {PopularBusinessList.map((item, index) => (
-          <div key={index} className="shadow-md rounded-lg">
+          <div
+            key={index}
+            className="shadow-md rounded-lg hover:shadow-lg hover:shadow-primary cursor-pointer hover:scale-105 transition-all ease-in-out"
+          >
             <img
               src={item.images[0]}
               alt={item.name}
@@ -14,13 +18,14 @@ const BusinessList = () => {
               height={200}
               className="object-cover h-[150px] md:h-[200px] rounded-lg"
             />
-            <div className="flex flex-col items-baseline p-3">
+            <div className="flex flex-col items-baseline p-3 gap-1">
               <h2 className="py-1 bg-purple-200 text-primary rounded-full px-2 text-sm">
                 {item.cateory.name}
               </h2>
               <h2 className="font-bold text-lg">{item.name}</h2>
               <h2 className="text-primary">{item.contactPerson}</h2>
               <h2 className="text-gray-500 text-sm">{item.address}</h2>
+              <Button className="mt-3">Book now</Button>
             </div>
           </div>
         ))}
