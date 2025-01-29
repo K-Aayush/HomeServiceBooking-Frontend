76faa21c0b57a7440a2679b2464632e989ba04eb
Aayush <ayushkarkee6@gmail.com>
2025-01-29 12:42:03 +0545

    update :: created component named businessList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import CategoryList from "../components/CategoryList";
+import BusinessList from "../components/BusinessList";
 
 const Home = () => {
   return (
     <div className="mx-6 md:mx-16">
       <Hero />
       <CategoryList />
+      <BusinessList />
     </div>
   );
 };
