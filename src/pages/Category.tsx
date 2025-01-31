4938ebf3133a3edbf510dfce33e17d12839c026d
Aayush <ayushkarkee6@gmail.com>
2025-01-31 15:45:37 +0545

    update :: adding grid layout for filtering categories

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -2,7 +2,12 @@ import { useParams } from "react-router-dom";
 
 const Category = () => {
   const { id } = useParams();
-  return <div>Categoryid: {id}</div>;
+  return (
+    <div className="grid grid-cols-4">
+      <div className="bg-blue-100">category</div>
+      <div className="col-span-3 bg-red-100">BusinessCategories</div>
+    </div>
+  );
 };
 
 export default Category;
