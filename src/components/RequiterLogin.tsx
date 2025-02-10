f4a38a9dab9bec9b282f466b9a262d6b028372ea
Aayush <ayushkarkee6@gmail.com>
2025-02-10 22:05:19 +0545

    update :: fixed code for register validation issue

--- src/components/RequiterLogin.tsx
+++ src/components/RequiterLogin.tsx
@@ -1,34 +1,96 @@
-import {
-  EyeIcon,
-  EyeOff,
-  ImageUp,
-  LockIcon,
-  MailIcon,
-  User,
-} from "lucide-react";
-import { useState } from "react";
+import { EyeIcon, EyeOff, LockIcon, MailIcon, User } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import image from "../assets/upload.png";
+import { requiterFormData, requiterFormSchema } from "../lib/validator";
 
 const RequiterLogin = () => {
   const [state, setState] = useState<string>("Login");
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [formValues, setFormValues] = useState<requiterFormData | null>(null);
   const [isTextDataSubmitted, setIsTextDataSubmitted] =
     useState<boolean>(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    setError,
+    clearErrors,
+  } = useForm<requiterFormData>({
+    defaultValues: {
+      companyName: "",
+      email: "",
+      password: "",
+      companyLogo: null,
+    },
+    resolver: zodResolver(requiterFormSchema),
+    shouldUnregister: true,
+  });
 
-    if (state === "Sign up" && !isTextDataSubmitted) {
-      setIsTextDataSubmitted(true);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {      
+      setValue("companyLogo", files, { shouldValidate: true });
+      console.log("file selected:", files[0]);
     }
   };
+
+  const onSubmit: SubmitHandler<requiterFormData> = async (data) => {
+    console.log("Form Data Before Processing:", { state, data });
+    if (state === "Sign up" && !isTextDataSubmitted) {
+      setIsTextDataSubmitted(true);
+      setFormValues(data);
+      console.log("Proceeding to Step 2 (File Upload)");
+      return;
+    }
+
+    if (state === "Sign up" && isTextDataSubmitted) {
+      if (!data.companyLogo || data.companyLogo.length === 0) {
+        setError("companyLogo", { message: "Company Logo is required" });
+
+        return;
+      }
+
+      const file = data.companyLogo[0];
+      console.log("Uploaded File:", file);
+      if (file.size > 5000000) {
+        setError("companyLogo", { message: "File size must be less than 5MB" });
+        return;
+      }
+
+      if (!["image/jpg", "image/png", "image/jpeg"].includes(file.type)) {
+        setError("companyLogo", { message: "Invalid file formate" });
+        return;
+      }
+
+      console.log("final submission:", data);
+    } else if (state === "Login") {
+      clearErrors("companyName");
+      console.log("Login SUbmission:", data);
+    }
+  };
+
+  useEffect(() => {
+    if (isTextDataSubmitted && formValues) {
+      setValue("companyName", formValues.companyName);
+      setValue("email", formValues.email);
+      setValue("password", formValues.password);
+    }
+  }, [isTextDataSubmitted, formValues, setValue]);
+
+  useEffect(() => {
+    console.log("⚠️ Form Errors:", errors);
+  }, [errors]);
+
   return (
     <div className="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center backdrop-blur-sm bg-black/30">
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="relative p-10 bg-white rounded-xl text-slate-500"
       >
         <h1 className="text-2xl font-medium text-center text-neutral-700">
@@ -41,7 +103,12 @@ const RequiterLogin = () => {
             <div className="flex items-center gap-4 my-10">
               <label htmlFor="image">
                 <img src={image} alt="image" className="w-16 rounded-full" />
-                <input type="file" id="image" hidden={true} />
+                <input
+                  type="file"
+                  id="image"
+                  hidden={true}
+                  onChange={handleFileChange}
+                />
               </label>
               <p>
                 Upload Company <br /> logo
@@ -52,45 +119,70 @@ const RequiterLogin = () => {
           <>
             {/* normal form for both  */}
             {state !== "Login" && (
-              <div className="relative mt-5">
-                <User className="absolute w-5 h-5 top-1/4 left-3" />
-                <Input
-                  placeholder="Company Name"
-                  type="text"
-                  required
-                  className="pl-10 rounded-full"
-                />
+              <div>
+                <div className="relative mt-5">
+                  <User className="absolute w-5 h-5 top-1/4 left-3" />
+                  <Input
+                    {...(state === "Sign up" ? register("companyName") : {})}
+                    placeholder="Company Name"
+                    type="text"
+                    className={`pl-10 rounded-full ${
+                      errors.companyName &&
+                      "border-red-500 focus-visible:ring-red-500"
+                    }`}
+                  />
+                </div>
+                {errors.companyName && (
+                  <p className="text-xs text-red-500">
+                    {errors.companyName.message}
+                  </p>
+                )}
               </div>
             )}
-            <div className="relative mt-5">
-              <MailIcon className="absolute w-5 h-5 top-1/4 left-3" />
-              <Input
-                placeholder="Email"
-                type="text"
-                required
-                className="pl-10 rounded-full"
-              />
+            <div>
+              <div className="relative mt-5">
+                <MailIcon className="absolute w-5 h-5 top-1/4 left-3" />
+                <Input
+                  {...register("email")}
+                  placeholder="Email"
+                  type="text"
+                  className={`pl-10 rounded-full ${
+                    errors.email && "border-red-500 focus-visible:ring-red-500"
+                  }`}
+                  required
+                />
+              </div>
+              {errors.email && (
+                <p className="text-xs text-red-500">{errors.email.message}</p>
+              )}
             </div>
-            <div className="relative mt-5">
-              <LockIcon className="absolute w-5 h-5 top-1/4 left-3" />
-              <Input
-                placeholder="Password"
-                type={showPassword ? "text" : "password"}
-                required
-                className="pl-10 rounded-full"
-              />
-
-              {/* state to show and hide password */}
-              {showPassword ? (
-                <EyeIcon
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute w-5 h-5 cursor-pointer top-1/4 right-3"
+            <div>
+              <div className="relative mt-5">
+                <LockIcon className="absolute w-5 h-5 top-1/4 left-3" />
+                <Input
+                  {...register("password")}
+                  placeholder="Password"
+                  type={showPassword ? "text" : "password"}
+                  className="pl-10 rounded-full"
+                  required
                 />
-              ) : (
-                <EyeOff
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute w-5 h-5 cursor-pointer top-1/4 right-3"
-                />
+                {/* state to show and hide password */}
+                {showPassword ? (
+                  <EyeIcon
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute w-5 h-5 cursor-pointer top-1/4 right-3"
+                  />
+                ) : (
+                  <EyeOff
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute w-5 h-5 cursor-pointer top-1/4 right-3"
+                  />
+                )}
+              </div>
+              {errors.password && (
+                <p className="text-xs text-red-500">
+                  {errors.password.message}
+                </p>
               )}
             </div>
           </>
@@ -101,7 +193,11 @@ const RequiterLogin = () => {
         </p>
 
         {/* button state ternery operator */}
-        <Button type="submit" className="w-full rounded-full">
+        <Button
+          type="submit"
+          className="w-full rounded-full"
+          onClick={() => console.log("button clicked state: ", state)}
+        >
           {state === "Login"
             ? "Login"
             : isTextDataSubmitted
--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 
+export type requiterFormData = z.infer<typeof requiterFormSchema>;
+
 export const requiterFormSchema = z.object({
   companyName: z
-    .string({ required_error: "Company Name is required" })
+    .string()
     .min(3, "Must be 3 or more characters long")
-    .max(20, "Must be less than 20 characters"),
+    .max(20, "Must be less than 20 characters")
+    .optional(),
   email: z.string({ required_error: "Email is required" }),
   password: z
     .string({ required_error: "Password is required" })
@@ -23,11 +26,5 @@ export const requiterFormSchema = z.object({
       (password) => /[!@#$%^&*]/.test(password),
       "Must contain one special character"
     ),
-  companyLogo: z
-    .any()
-    .refine((file) => file?.[0]?.size < 500000, "File can't be bigger than 5MB")
-    .refine(
-      (file) => ["image/jpg", "image/png", "image/jepg"].includes(file.type),
-      "file formate must be either jpg, jepg or png"
-    ),
+  companyLogo: z.any().optional(),
 });
