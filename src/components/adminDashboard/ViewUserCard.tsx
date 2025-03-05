973de254c858594e17e7481b8fe5362f48535077
Aayush <ayushkarkee6@gmail.com>
2025-03-06 02:44:11 +0545

    upadate :: created the total no of users page

--- src/components/adminDashboard/ViewUserCard.tsx
+++ src/components/adminDashboard/ViewUserCard.tsx
@@ -1,12 +1,11 @@
 import { Card, CardHeader, CardTitle } from "../ui/card";
 
-const ViewUserCard = ({ totalUsers }) => {
+const ViewUserCard = ({ totalUsers, title }) => {
   return (
-    <Card className="flex flex-col items-center justify-between transition-transform duration-300 ease-in-out cursor-pointer hover:scale-105">
-      <CardHeader>
-        <CardTitle className="text-xl">
-          Total no of users: {totalUsers}
-        </CardTitle>
+    <Card className="flex flex-col items-center justify-between p-4 transition-transform duration-300 ease-in-out cursor-pointer hover:bg-gray-100">
+      <CardHeader className="flex items-center">
+        <CardTitle className="text-xl font-semibold">{title}</CardTitle>
+        <p className="text-lg font-medium">{totalUsers ?? 0}</p>
       </CardHeader>
     </Card>
   );
--- src/pages/AdminDashboard/ViewAdminDashboard.tsx
+++ src/pages/AdminDashboard/ViewAdminDashboard.tsx
@@ -3,11 +3,13 @@ import { AppContext } from "../../context/AppContext";
 import ViewUserCard from "../../components/adminDashboard/ViewUserCard";
 
 const ViewAdminDashboard = () => {
-  const { error, isLoading, totalUsers } = useContext(AppContext);
+  const { isLoading, totalUsers } = useContext(AppContext);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {isLoading ? <p>Loading...</p> : <ViewUserCard totalUsers={totalUsers} />}
+    <div className="grid grid-cols-1 gap-3 mx-4 md:grid-cols-2 lg:grid-cols-3">
+      <ViewUserCard totalUsers={totalUsers.total} title="Total Users" />
+      <ViewUserCard totalUsers={totalUsers.user} title="Total Customers" />
+      <ViewUserCard totalUsers={totalUsers.requiter} title="Total Recruiters" />
     </div>
   );
 };
