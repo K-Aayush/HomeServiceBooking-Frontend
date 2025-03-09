8484345a2a14089ee276273c821f8998fdd8c0f9
Aayush <ayushkarkee6@gmail.com>
2025-03-09 16:40:36 +0545

    viewing usertable

--- src/pages/AdminDashboard/ViewUsers.tsx
+++ src/pages/AdminDashboard/ViewUsers.tsx
@@ -1,5 +1,48 @@
-const ViewUsers = () => {
-  return <div>ViewUsers</div>;
+import { useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import ViewUserTable from "../../components/admin/ViewUserTable";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select";
+
+const ManageUsers = () => {
+  const { error, isLoading, users } = useContext(AppContext);
+  const [selectedRole, setSelectedRole] = useState<keyof typeof users>("total");
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p className="text-red-500">{error}</p>;
+
+  return (
+    <div className="p-4 mx-4">
+      <Select
+        defaultValue={selectedRole}
+        onValueChange={(value) => setSelectedRole(value as keyof typeof users)}
+      >
+        <SelectTrigger className="w-[200px]">
+          <SelectValue>
+            {selectedRole === "total" ? "All Users" : selectedRole}
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="total">All Users</SelectItem>
+            <SelectItem value="users">Customers</SelectItem>
+            <SelectItem value="requiter">Requiters</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+      <div className="mt-4">
+        <ViewUserTable users={users[selectedRole]} />
+      </div>
+    </div>
+  );
 };
 
-export default ViewUsers;
+export default ManageUsers;
