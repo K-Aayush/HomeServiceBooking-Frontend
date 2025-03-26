c992c46e237b8a32b015bedc69338a26f985dbbf
Aayush <ayushkarkee6@gmail.com>
2025-03-26 20:55:34 +0545

    using the tabs component in bookings page

--- src/pages/MyBooking.tsx
+++ src/pages/MyBooking.tsx
@@ -1,5 +1,25 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../components/ui/tabs";
+
 const MyBooking = () => {
-  return <div>MyBooking</div>;
+  return (
+    <div className="min-h-screen mx-5 my-10 md:mx-36">
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default MyBooking;
