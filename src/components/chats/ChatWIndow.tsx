6d6923231e8ceee63967151ea75fb09b826a946f
Aayush <ayushkarkee6@gmail.com>
2025-04-09 22:18:06 +0545

    importing necessary imports and adding types in chat window

--- src/components/chats/ChatWIndow.tsx
+++ src/components/chats/ChatWIndow.tsx
@@ -1,4 +1,27 @@
-import React from "react";
+import type React from "react";
+
+import { Send } from "lucide-react";
+import { Button } from "./ui/button";
+import { Skeleton } from "./ui/skeleton";
+import { format } from "date-fns";
+
+interface Message {
+  id: string;
+  content: string;
+  senderId: string;
+  senderType: "USER" | "REQUITER";
+  createdAt: string;
+}
+
+interface ChatWindowProps {
+  messages: Message[];
+  newMessage: string;
+  setNewMessage: (message: string) => void;
+  handleSendMessage: () => void;
+  isUser: boolean;
+  currentId: string;
+  loading: boolean;
+}
 
 const ChatWindow = () => {
   return <div>ChatWIndow</div>;
